285d35d440945a099f610dd499ef69f6b3f27027
andrewchiang8 <andrewchiang8@gmail.com>
2020-03-02 17:15:07 -0500

    idk

--- hw_lexicon_entity_matching.py
+++ hw_lexicon_entity_matching.py
@@ -63,27 +63,33 @@ df.set_error_successor(State.TURN0, State.TURN0ERR)
 df.add_system_transition(State.TURN0ERR, State.TURN0, "I have never heard of them. Please enter a current player") #todo this turn to, what state seems rather abrupt, see if there is a way to make it more smooth
 
 #turn 1
-df.add_system_transition(State.TURN1S, State.TURN1U, '"Here is what I know about $team. #news($team) What do you think about this situation?"')
+df.add_system_transition(State.TURN1S, State.TURN1U, '"Here is what I know about $team. What do you think about this situation?"')
+# df.add_system_transition(State.TURN1S, State.TURN1U, '"Here is what I know about $team. #news($team) What do you think about this situation?"')
 df.add_user_transition(State.TURN1U, State.TURN2S, "[$response1]") #todo here we could have system detect if user thinks the idea is good or bad
-df.set_error_successor(State.TURN1U, State.TURN1ERR, "I have heard that a lot of people have similar opinions to that")
-df.set_system_transition(State.TURN1ERR, State.TURN2S) #todo this might be wrong
 
-#turn 2
-df.add_system_transition(State.TURN2S, State.TURN2U, "[!I think that is ", "#macro()", "I also heard that " "[#macro2()] ", "What do you think of that?") #todo macro here would be hard coded to always be positive view of the news
-df.add_system_transition(State.TURN2U, State.TURN3S, "[$response2]")
-df.set_error_successor(State.TURN2U, State.TURN2ERR, "That is a pretty interesting take, I have not heard that before")
-df.add_system_transition(State.TURN2ERR, State.TURN3S)
+#### FOR TESTING DELETE AFTER
+df.add_system_transition(State.TURN2S, State.END, 'goodbye') #todo this might be wrong
+############################
 
-#turn3
-df.add_system_transition(State.TURN3S, State.TURN3U, "I agree with your take. This move also definetly impacts the ", "#macro, ", "how big of a change do you think this will be?")
-df.add_user_transition(State.TURN3U, State.TURN4S, "[$response3]")
-df.set_error_successor(State.TURN3U, State.TURN3ERR, "")
+# df.set_error_successor(State.TURN1U, State.TURN1ERR, "I have heard that a lot of people have similar opinions to that")
+# df.set_system_transition(State.TURN1ERR, State.TURN2S) #todo this might be wrong
 
-df.add_system_transition(State.TURN3ERR, State.TURN3S) #todo need to fix this error handling, this is temporary
+# #turn 2
+# df.add_system_transition(State.TURN2S, State.TURN2U, "[!I think that is ", "#macro()", "I also heard that " "[#macro2()] ", "What do you think of that?") #todo macro here would be hard coded to always be positive view of the news
+# df.add_system_transition(State.TURN2U, State.TURN3S, "[$response2]")
+# df.set_error_successor(State.TURN2U, State.TURN2ERR, "That is a pretty interesting take, I have not heard that before")
+# df.add_system_transition(State.TURN2ERR, State.TURN3S)
+
+# #turn3
+# df.add_system_transition(State.TURN3S, State.TURN3U, "I agree with your take. This move also definetly impacts the ", "#macro, ", "how big of a change do you think this will be?")
+# df.add_user_transition(State.TURN3U, State.TURN4S, "[$response3]")
+# df.set_error_successor(State.TURN3U, State.TURN3ERR, "")
+
+# df.add_system_transition(State.TURN3ERR, State.TURN3S) #todo need to fix this error handling, this is temporary
 
 
-#turn 4
-df.add_system_transition(State.TURN4S, END, "[! I actually have not decided myself how important this move is. How about I talk to you in a couple of day about that?")
+# #turn 4
+# df.add_system_transition(State.TURN4S, END, "[! I actually have not decided myself how important this move is. How about I talk to you in a couple of day about that?")
 
 
 
