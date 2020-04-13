cfa40c61381a38fcd97eb86d2b61972d4d4b4110
andrewchiang8 <andrewchiang8@gmail.com>
2020-04-13 01:01:41 -0400

    added macro and deleted accessingapi

--- hw_lexicon_entity_matching.py
+++ hw_lexicon_entity_matching.py
@@ -78,11 +78,6 @@ ontology = {
         }
 }
 
-#GLOBAL VARS????
-receivingTeam = str()
-givingTeam = str()
-player = str()
-
 class news(Macro):
     def run (self, ngrams, vars, args):
         #andrew- im just gonna assume that the input is the team name and only the team name, eg "Atlanta Hawks"
@@ -264,6 +259,37 @@ class tradeNews(Macro):
             data = json.load(f)
         trades = data['trades']
         trade = trades[randrange(55)]['TRANSACTION_DESCRIPTION']
+        receivingTeam = trade.split(' received')[0]
+        givingTeam = trade.split('from ')[1]
+        givingTeam = givingTeam[:-1]
+        player = trade.split('received ')[1]
+        player = player.split('from')[0]
+
+        playerList = player.split(' ')
+        role = playerList[0]
+        playerList.pop(0)
+        player = ' '.join(playerList)
+
+        vars['receivingTeam'] = receivingTeam
+        vars['givingTeam'] = givingTeam
+        vars['player'] = player
+        
+        #print(trade)
+        #print('recieving team', receivingTeam)
+        #print('givingTeam', givingTeam)
+        #print(player)
+        #print(role)
+
+        return "I found this most recent trade news that {} from {} is going to {}".format(player, givingTeam, receivingTeam)
+
+class tradeNewsByTeam(Macro):
+    def run (self, ngrams, vars, args):
+        with open('trades.json') as f:
+            data = json.load(f)
+        trades = data['trades']
+        trades = [x for x in trades if vars['botFavTeam'] in x['TRANSACTION_DESCRIPTION']]
+        trade = trades[randrange(len(trades))]['TRANSACTION_DESCRIPTION']
+        
         receivingTeam = trade.split(' received')[0]
         givingTeam = trade.split('from ')[1]
         givingTeam = givingTeam[:-1]
